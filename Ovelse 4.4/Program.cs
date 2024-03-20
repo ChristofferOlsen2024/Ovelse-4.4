c5d764ed864bb8f5e64d579fb08f098d5f8ab90b
Christoffer Olsen <chris70944@gmail.com>
2024-03-20 22:04:04 +0100

    Add project files.

---
+++ Ovelse 4.4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Vil du have ananas på din pizza?");
+
+string svar = Console.ReadLine();
+
+if (svar == "ja")
+{
+    Console.WriteLine("Det skal du få.");
+} if else (svar == "nej")
+{
+    Console.WriteLine("Okay det får du ik så.");
+}
